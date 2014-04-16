2a3606e949c6902913a17e795f6c6294b91bdd6e
Anisimov Egor <egoran@inbox.ru>
2014-04-16 18:40:12 +0400

    In ColorShaderClass OutputShaderErrorMessage and SetShaderParameters were added

--- Engine/ColorShaderClass.cpp
+++ Engine/ColorShaderClass.cpp
@@ -9,7 +9,7 @@ ColorShaderClass::ColorShaderClass()
 
 	m_worldMatrixPtr = 0;
 	m_viewMatrixPtr = 0;
-	m_progectionMatrixPtr = 0;
+	m_projectionMatrixPtr = 0;
 }
 
 
@@ -148,6 +148,53 @@ void ColorShaderClass::ShutdownShader()
 		m_effect->Release();
 		m_effect = 0;
 	}
+
+	return;
+}
+
+void ColorShaderClass::OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, 
+												WCHAR* shaderFilename)
+{
+	char* compileErrors;
+	unsigned long bufferSize, i;
+	ofstream fout;
+
+	// Get a pointer to the error message text buffer.
+	compileErrors = (char*)(errorMessage->GetBufferPointer());
+
+	// Get the length of the message.
+	bufferSize = errorMessage->GetBufferSize();
+
+	// Open a file to write an error message to
+	fout.open("shader-error.txt");
+
+	// Write out the error message.
+	for (i=0; i<bufferSize; i++)
+	{
+		fout << compileErrors[i];
+	}
+
+	// Close the file
+	fout.close();
+
+	// Release the error message.
+	errorMessage->Release();
+	errorMessage = 0;
+
+	// Pop a message up on the screen to notify the user to check the text file for compile errors.
+	MessageBox(hwnd, L"Error compiling shader. Check shader-error.txt for message.", 
+			shaderFilename, MB_OK);
+
+	return;
+}
+
+void ColorShaderClass::SetShaderParameters(D3DXMATRIX worldMatrix, D3DXMATRIX viewMatrix, 
+										   D3DXMATRIX projectionMatrix)
+{
+	// Set matrices inside the shader
+	m_worldMatrixPtr->SetMatrix((float*)&worldMatrix);
+	m_viewMatrixPtr->SetMatrix((float*)&viewMatrix);
+	m_projectionMatrixPtr->SetMatrix((float*)&projectionMatrix);
 
 	return;
 }
